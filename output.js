f3dcc5fec9f612a625f1b5be8b03c3b78fa479f2
trilogyletter <trionofaqoth11@gmail.com>
2023-01-07 16:21:55 +0700

    update data

---
+++ output.js
@@ -0,0 +1,5 @@
+// membuat objek elemen
+var hasil = document.getElementById("hasil-output");
+
+// menampilkan output ke elemen hasil
+hasil.innerHTML = "<p>Sedang konudusif belajar javascript</p>";
