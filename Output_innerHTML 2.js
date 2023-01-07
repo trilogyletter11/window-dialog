fc274036a10ad0b55083187c1d847ab11da17dd0
trilogyletter <trionofaqoth11@gmail.com>
2023-01-07 17:40:27 +0700

    updata data

---
+++ Output_innerHTML 2.js
@@ -0,0 +1,4 @@
+function status() {
+  document.getElementById("p").innerHTML =
+    "Sedang mempelajari Javascript dari dasar";
+}
---
+++ Output_innerHTML 3.js
@@ -0,0 +1,5 @@
+function status() {
+  var x = document.getElementById("P").innerHTML;
+  document.getElementById("p").innerHTML = x;
+  document.getElementById("p").style.color = "green";
+}
